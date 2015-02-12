315f591bffe1cef3414b57f0047b05caecb5849e
justinjaywang <justinjaywang@gmail.com>
2015-02-12 07:53:46 -0800

    built out more index styles; changed post to project in class and var names

--- themes/portfolio/source/js/all.js
+++ themes/portfolio/source/js/all.js
@@ -1,41 +1,40 @@
 +function ($) {
   'use strict';
 
-  $('.post-content').each(function (i, postContent) {
-    $(postContent).find('img').each(function () {
-      // for each <img>, add captions and add classes
-      var img = this,
-        $img = $(this);
+  $('.project-content').each(function (i, projectContent) {
+    $(projectContent).find('img').each(function () {
       // (1) wrap imgs in a figure
-      var $imgWrap = $img.parent();
+      var img = this,
+        $img = $(this),
+        $imgWrap = $img.parent();
       if ($imgWrap.is('p')) {
         $imgWrap.find('br').remove(); // remove extra <br>
         $img.unwrap(); // remove <p> wrap
       }
-      $img.wrap('<figure class="post-figure"></figure>');
+      $img.wrap('<figure class="project-figure"></figure>');
       // (2) append figcaption
-      var alt = img.alt;
-      var $figure = $img.parent();
+      var alt = img.alt,
+        $figure = $img.parent();
       if (alt) {
-        $figure.append('<figcaption class="post-figure-caption">' + alt + '</figcaption>');
+        $figure.append('<figcaption class="project-figure-caption">' + alt + '</figcaption>');
       }
-      // (3) assign proper class based on image type
+      // (3) assign proper CSS class based on image type
       if ($img.hasClass('wide')) { // wide image type
-        $img.parent().addClass('post-figure--wide');
+        $img.parent().addClass('project-figure--wide');
       } else if ($img.hasClass('full')) {
-        $img.parent().addClass('post-figure--full');
+        $img.parent().addClass('project-figure--full');
       } else if ($img.hasClass('left')) { // left-floated image type
-        $img.parent().addClass('post-figure--left');
+        $img.parent().addClass('project-figure--left');
         $img.parent().wrap('<div class="container--narrow"></div>');
       } else { // default image type
-        $img.parent().addClass('post-figure--default');
+        $img.parent().addClass('project-figure--default');
       }
     });
   });
   
   $(window).on('load pageshow', function() {
-    // // colorize per post
-    // var color = $(document.getElementById('postColor')).html();
+    // // colorize per project
+    // var color = $(document.getElementById('projectColor')).html();
     // if (color) {
     //   $('.page-header').find('.colorize').each(function () {
     //     $(this).css('color', color);
